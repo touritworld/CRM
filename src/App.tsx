5b9ae0d22640f00858a49f9f94344e451e8f0296
propiter <pro.pedro0413@gmail.com>
2024-11-20 21:01:06 -0500

    a

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import BookingForm from "./pages/BookingForm";
+import Blog from "./pages/Blog";
+import AdminDashboard from "./pages/AdminDashboard";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<AdminDashboard />} />
+            <Route path="/book" element={<BookingForm />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+          </Routes>
+        </main>
+        <Footer />
+        <Toaster position="top-right" />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
